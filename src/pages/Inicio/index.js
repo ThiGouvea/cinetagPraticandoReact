2a679c3cd315b633a18455b2b664d80f61499bad
Thiagouvea <thiagouvea90@gmail.com>
2023-11-10 14:55:21 -0300

    adicionado tela de favoritos

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,5 @@
 import Banner from "components/Banner"
-import Cabecalho from "components/Cabecalho"
 import Card from "components/Card"
-import Rodape from "components/Rodape"
 import Titulo from "components/Titulo"
 import videos from 'json/db.json'
 import styles from "./Inicio.module.css"
@@ -9,19 +7,17 @@ import styles from "./Inicio.module.css"
 function Inicio() {
     return(
         <>
-            <Cabecalho/>
             <Banner imagem="home"/>
             <Titulo>
                 <h1>
                     Um lugar para guardar seus videos e filmes!
                 </h1>
             </Titulo>
-            <section className={styles.contairner}>
+            <section className={styles.container}>
                 {videos.map((video) => {
                     return (<Card {...video} key={video.id} />)
                 })}
             </section>
-            <Rodape />
         </>
     )
 }
