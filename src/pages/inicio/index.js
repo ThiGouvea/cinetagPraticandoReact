1fa177deea382262f306c44d642f93ce10b038da
Thiagouvea <thiagouvea90@gmail.com>
2023-11-10 09:03:58 -0300

    adcionado cards

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,20 @@
 import Banner from "components/Banner"
 import Cabecalho from "components/Cabecalho"
+import Card from "components/Card"
 import Rodape from "components/Rodape"
+import Titulo from "components/Titulo"
 
 function Inicio() {
     return(
         <>
             <Cabecalho/>
             <Banner imagem="home"/>
+            <Titulo>
+                <h1>
+                    Um lugar para guardar seus videos e filmes!
+                </h1>
+            </Titulo>
+            <Card id='1' titulo='Gato Bonifacio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
             <Rodape />
         </>
     )
